50d50fb477cfd6d2c9ef156479937ef952ed30cf
tanuki77 <inadayutaka07@gmail.com>
2021-02-06 15:06:01 +0900

    稲田のログイン部分です

---
+++ save.php
@@ -0,0 +1,24 @@
+<?php
+require_once('funcs.php');
+
+$name = $_POST["name"];
+$pass = $_POST["pass"];
+$position = $_POST["position"];
+
+$db = connectDB();
+
+$stmt = $db->prepare('INSERT INTO user_table(id, name, pass, position)VALUES(NULL,:name,:pass,:position)');
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);
+$stmt->bindValue(':pass', $pass, PDO::PARAM_STR);
+$stmt->bindValue(':position', $position, PDO::PARAM_STR);
+
+$status = $stmt->execute();
+
+echo 'aaa';
+
+if ($status == false) {
+    echo 'error';
+    echo '<a href="userSave.php">戻る</a>';
+} else {
+    header('Location: userSave.php');
+}
