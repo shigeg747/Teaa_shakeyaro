6dddefab2c2eb7c56f4b401ccbcd9ade0a834550
tanuki77 <inadayutaka07@gmail.com>
2021-02-07 17:09:03 +0900

    最新の綺麗なバージョンVer

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 window.onload = function () {
   var SE = document.getElementById("se");
   SE.play();
 };
-=======
-window.onload = function(){
-    var SE = document.getElementById('se');
-    SE.play();
-    }
->>>>>>> 7a5a0a2e1338d480117b40396e4124f5acf8e815
